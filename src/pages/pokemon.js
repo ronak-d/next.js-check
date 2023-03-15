fbea1d39769d25861fd79af170c9b701912d64e0
ronak-d <ronakdodeja73@gamil.com>
2023-03-15 18:26:10 +0530

    mapping issue solved thaks stackoverflow😭

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -1,32 +1,31 @@
 import PageComp from "@/Components/Pagination/PageComp";
 import SearchComp from "@/Components/Searchbar/SearchComp";
 import TableComp from "@/Components/Table/TableComp";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import index from ".";
 
-export const getStaticProps = async () => {
-  const res = await axios.get("https://pokeapi.co/api/v2/ability/");
-  // const data = await res.json();
-
-  return {
-    props: {
-      data: res,
-    },
-  };
-};
-
-const Pokemon = ({ data }) => {
-  console.log(data);
+const Pokemon = ({ pokemons }) => {
+  console.log(pokemons);
   return (
     <>
       <div>
-        <SearchComp />
-        <TableComp />
-        <div>Pokemon page</div>
-        <PageComp />
+        {pokemons.results?.map((e) => (
+          <h3>{e.name}</h3>
+        ))}
       </div>
     </>
   );
 };
 
 export default Pokemon;
+
+export async function getStaticProps() {
+  const res = await axios.get("https://pokeapi.co/api/v2/ability/");
+  const data = res.data;
+  return {
+    props: {
+      pokemons: data,
+    },
+  };
+}
