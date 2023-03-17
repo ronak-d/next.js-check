975a8911d13e3ea93e735827557f3ad6f36b7aa3
ronak-d <ronakdodeja73@gamil.com>
2023-03-17 15:45:11 +0530

    getstaticpaths for dyanmic and remember context!

--- src/pages/PokemonPage/[PokemonName].js
+++ src/pages/PokemonPage/[PokemonName].js
@@ -1,22 +1,45 @@
 import React from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
-const Pokemonid = () => {
+const Pokemonid = ({ SinglepokemonData }) => {
   const router = useRouter();
   const PokemonName = router.query.PokemonName;
 
-  return <div>The Pokemon {PokemonName}</div>;
+  // const { name } = SinglepokemonData;
+  console.log(SinglepokemonData.name);
+
+  return <div>The Pokemon {SinglepokemonData.name}</div>;
 };
 
 export default Pokemonid;
 
-export async function getStaticProps() {
-  // const res = await axios.get("https://pokeapi.co/api/v2/ability/");
+export async function getStaticPaths() {
   const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
   const data = res.data;
+
+  const paths = data.results.map((currentPokem) => {
+    return {
+      params: {
+        PokemonName: currentPokem.name,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const name = context.params.PokemonName;
+  const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const data = res.data;
+
   return {
     props: {
-      pokemons: data,
+      SinglepokemonData: data,
     },
   };
 }
