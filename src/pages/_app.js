822850b28c93e1f27f807f1f0e02ca846a68fbce
ronak-d <ronakdodeja73@gamil.com>
2023-03-19 16:47:20 +0530

    err agin solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import Nav from "@/Components/Navbar/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
-    // dark mode => dark:bg-slate-900 dark:text-white
     <div className="dark:bg-slate-900 dark:text-white">
       <Nav />
       <div>
